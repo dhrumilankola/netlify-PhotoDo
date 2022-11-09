f2ad505f169b62e3de948dba75871e958a88efca
dhrumilankola <70380989+dhrumilankola@users.noreply.github.com>
2022-11-09 23:42:00 +0530

    First commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './footer.scss';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            Created with  <span> ❤ </span> By Dhrumil Ankola
+        </div>
+    )
+}
+
+export default Footer
